72a92804e26e46627b8f442053f5504b45d66004
Leyla Khairnabiyeva <leylaskh@code.edu.az>
2024-12-06 18:48:38 +0400

    our process section created

--- src/script/app.js
+++ src/script/app.js
@@ -125,3 +125,27 @@ getAPIdata(dataQuery.statistics, (data) => {
 });
 
 ///////statistics
+
+//////scoll
+document.addEventListener("DOMContentLoaded", () => {
+  const images = document.querySelectorAll(".our_process_img");
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("active");
+        } else {
+          entry.target.classList.remove("active");
+        }
+      });
+    },
+    {
+      threshold: 0.5,
+    }
+  );
+
+  images.forEach((img) => observer.observe(img));
+});
+
+//////scoll
